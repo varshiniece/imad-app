69f589a41b0766f9ce98637cb8985fae2eb1b9fb
varshiniece <30771472+varshiniece@users.noreply.github.com>
2017-08-25 20:10:55 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -18,7 +18,9 @@ app.get('/article-two',function(req,res){
     res.sendFile(path.join(__dirname, 'ui', 'article-two.html'));
 })
 app.get('/article-three',function(req,res){
-    res.send("article THREE is requested and will be served here");
+    //res.send("article THREE is requested and will be served here");
+    res.sendFile(path.join(__dirname, 'ui', 'article-three.html'));
+    
 })
 
 
