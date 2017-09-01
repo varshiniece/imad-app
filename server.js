6821080a63dfaac3a1ec9f81423438eeb5ec735a
varshiniece <30771472+varshiniece@users.noreply.github.com>
2017-09-01 12:24:47 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -5,6 +5,12 @@ var path = require('path');
 var app = express();
 app.use(morgan('combined'));
 
+var content = {
+    title : 'Article one | Varshini Ch',
+    
+    
+}
+
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
